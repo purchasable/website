d3ff659b1af260e0314ad113946bcf5e2016a42f
Joseph <jcmurphy.business@gmail.com>
2022-09-10 20:55:38 +0100

    some tweaks

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,29 +1,50 @@
 import React from "react";
 import PageFooter from "./components/footer";
+import Pic from "../public/me.png";
+import Image from "next/image";
 
 const about = () => {
     return (
-        <div className="font-semibold lg:font-normal">
-            <div className="lg:p-20 lg:items-start items-center flex flex-col justify-center px-5 py-20 text-xs lg:text-sm space-y-6 text-white">
-                <h1 className="text-xl lg:text-2xl text-blue-500 font-bold">
+        <div className="font-semibold xl:font-normal static bg-zinc-900">
+            <div className="xl:px-20 xl:pt-20 pt-0 xl:items-start items-center flex flex-col justify-center px-5 pt-12 text-xs md:text-sm space-y-6 text-white">
+                <h1 className="text-xl xl:text-2xl text-blue-500 font-bold">
                     About
                 </h1>
-                <span className="lg:mr-[30%]">
+                <span className="xl:mr-[30%]">
                     Hey! I'm a 17 y/o full-stack software developer from the
                     United Kingdom. I've been programming for just under 3
                     years; in which time I've picked up many different skills,
                     not exclusively programming based, but also more fundamental
                     lessons on how to tackle problems.
                 </span>
-                <span className="lg:mr-[30%]">
+                <span className="xl:mr-[30%]">
                     Aside from that, I'm a pretty normal teenager; I love to
                     travel, listen to music, go out with my friends, and of
                     course, struggle to manage my time...
                 </span>
-                <span className="lg:mr-[30%]">
+                <span className="xl:mr-[30%]">
                     I hope to continue building my knowledge of programming in
-                    all aspects, in order to create the most efficient &
-                    scalable code possible.
+                    all aspects, in order to continue creating the most scalable
+                    & efficient code possible.
+                </span>
+            </div>
+            <div className="xl:px-20 xl:items-start items-center flex flex-col justify-center px-5 pb-12 pt-6 text-xs xl:text-sm space-y-2 xl:space-y-4 text-white">
+                <Image
+                    src={Pic}
+                    width={800}
+                    height={400}
+                    className="block rounded-lg object-cover text-white"
+                />
+                <span className="not-sr-only text-neutral-500">
+                    a picture of me and{" "}
+                    <a
+                        className="text-neutral-400 hover:text-blue-500 hover:cursor-pointer"
+                        href="https://www.instagram.com/cosimo_kind/"
+                        target={"_blank"}
+                    >
+                        cosimo
+                    </a>{" "}
+                    (london sept 2022)
                 </span>
             </div>
             <PageFooter />
